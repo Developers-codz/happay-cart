67d0219a2201ea7916afb3f55ae0a0304e4425be
developers-codz <kaj123al2002@gmail.com>
2022-09-01 22:23:53 +0530

    refactor:li to links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { Cart } from "../../assets";
 import "./navbar.css";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="nav-heading centered">
+      <Link to="/" className="nav-heading centered pointer">
         <img
           src="https://react-coding-assignment.s3.ap-south-1.amazonaws.com/cards/blue_logo.svg"
           alt="logo"
           className="logo-icon"
         />
         <div className="logo-name">Happay</div>
-      </div>
+      </Link>
       <ul className="centered nav-links">
-        <li className="cart-wrapper">
+        <Link  to="/order-summary" className="cart-wrapper pointer">
         <Cart />
         <div className="qty-badge">0</div>
-        </li>
+        </Link>
         <li><img src="https://images.unsplash.com/photo-1521227889351-bf6f5b2e4e37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YnJvd24lMjB3b21hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" className="profile-img" /></li>
       </ul>
     </nav>
