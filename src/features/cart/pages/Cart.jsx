3573aeca9e2e1f679c572531b2159333ee583887
developers-codz <kaj123al2002@gmail.com>
2022-09-02 09:26:22 +0530

    feat: cart design don

--- src/features/cart/pages/Cart.jsx
+++ src/features/cart/pages/Cart.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
+import "./cart.css";
+import { Link } from "react-router-dom";
 
 export const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <main className="cart-container">
+      <Link to="/" className="home-link">
+        Back to Home
+      </Link>
+      <div className="cart-head">Order Summary (3 Items)</div>
+      <div className="cards-wrapper centered">
+        <div className="cart-product-container">
+          <div className="cart-product-head">
+            <div className="s-no">S. NO.</div>
+            <div className="cart-items-head">ITEMS</div>
+            <div className="qty-head">QTY</div>
+          </div>
+          <ul>
+            <li className="decor-none distributed cart-product-card">
+              <div className="sno">1</div>
+              <div className="product-name">Food Cart</div>
+              <div className="qty-wrapper centered">
+                <button className="qty-btn-minus qty-btn pointer">-</button>
+                <input type="text" className="qty-input" disabled />
+                <button className="qty-btn-plus qty-btn pointer">+</button>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="order-summary-container">
+          <h2 className="order-head">Price Details</h2>
+        </div>
+      </div>
+    </main>
+  );
+};
